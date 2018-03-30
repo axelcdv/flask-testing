1125d8a88f5574ce59aee26095e83c0ba7586c0a
Axel Colin de Verdiere <axel.cdv@uptime.ac>
2018-03-30 09:35:36 +0200

    Add two other views with associated tests

--- project/core/tests/test_views.py
+++ project/core/tests/test_views.py
@@ -2,10 +2,28 @@ from project.base import BaseTestCase
 from . import factories
 
 
-class CoreViewsTestCase(BaseTestCase):
+class PostsViewsTestCase(BaseTestCase):
     def test_post_list(self):
         post = factories.PostFactory()
 
-        response = self.client.get('/')
+        response = self.client.get('/posts')
         assert response.status_code == 200
         assert post.title in response.data.decode('utf-8')
+
+    def test_post_detail(self):
+        post = factories.PostFactory()
+        self.db.session.add(post)
+        self.db.session.commit()
+
+        response = self.client.get(f'/posts/{post.id}')
+        assert response.status_code == 200
+        assert post.body in response.data.decode('utf-8')
+
+
+class AuthorsViewsTestCase(BaseTestCase):
+    def test_author_list(self):
+        author = factories.AuthorFactory()
+
+        response = self.client.get('/authors')
+        assert response.status_code == 200
+        assert author.first_name in response.data.decode('utf-8')
--- project/core/views.py
+++ project/core/views.py
@@ -2,13 +2,29 @@ from flask import render_template
 from . import core, models
 
 
-@core.route('/')
+@core.route('/authors')
+def author_list():
+    """
+    List of blog authors
+    """
+    authors = models.Author.query.all()
+
+    return render_template(
+        'author_list.html', authors=authors)
+
+
+@core.route('/posts')
 def post_list():
     """
     List of blog posts
     """
     posts = models.Post.query.all()
 
+    return render_template('post_list.html', posts=posts)
 
-    return render_template(
-        'post_list.html', posts=posts)
+
+@core.route('/posts/<int:post_id>')
+def post_detail(post_id):
+    post = models.Post.query.get_or_404(post_id)
+
+    return render_template('post_detail.html', post=post)
